e3bdcdeff58a2a73fb1c006a12b2fe6d2d378856
simme <simon.ljungberg@nimnim.se>
2010-10-06 08:28:01 +0200

    Began imlementing the sites resource

--- resources/sites.php
+++ resources/sites.php
@@ -1,2 +1,43 @@
 <?php
 // $Id$
+
+/**
+ * @file
+ * Site Resource
+ * Exposes the Ægir site functionality and let's you create new sites, modify
+ * existing sites etc.
+ */
+class SitesResource {
+
+  // -------------------------------------------------------------------------
+  //  C.R.U.D.I Methods below
+  // -------------------------------------------------------------------------
+
+  /**
+   *  Create a new site
+   *
+   * @access  public
+   * @static
+   * @param   object  $data   ["data"]
+   * @return  object
+   *
+   * @Access(callback='SitesResource::access', args={'create'}, appendArgs=true)
+   */
+  public static function create() {
+
+  }
+
+  /**
+   * Access callback
+   * Todo: implement
+   *
+   * @access  public
+   * @static
+   * @param   string  $op
+   * @param   object  $args
+   * @return  bool    $access
+   */
+  public static function access($op, $args) {
+    return TRUE;
+  }
+}
